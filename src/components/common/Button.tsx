ee7e8b862237c7eb413d8c2488cd5dfa0b0e4bb8
puzzledPublic <puzzledPublic@gmail.com>
2020-01-16 17:52:56 +0900

    change button color

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,24 +2,27 @@ import React from "react";
 import styled from "styled-components";
 
 interface ColorPallete {
-  [key: string]: string;
+  [key: string]: {color: string, hover: string};
 }
 
 const pallete: ColorPallete = {
-  light: "#f8f9fa",
-  dark: "#343a40",
-  primary: "#007bff",
-  secondary: "#6c757d",
-  warning: "#ffc107"
+  light: {color: "#f8f9fa", hover: "#bdc3c7"},
+  dark: {color: "#2c3e50", hover: "#34495e"},
+  primary: {color: "#2980b9", hover: "#3498db"},
+  secondary: {color: "#7f8c8d", hover: "#95a5a6"},
+  warning: {color: "#f39c12", hover: "#f1c40f"}
 };
 
 const ButtonBlock = styled.button<{ color: string }>`
-  background-color: ${prop => pallete[prop.color]};
+  background-color: ${prop => pallete[prop.color].color};
   border: none;
   border-radius: 5px;
   color: white;
   cursor: pointer;
   padding: 5px 10px;
+  :hover {
+    background-color: ${prop => pallete[prop.color].hover};
+  }
 `;
 
 interface ButtonProp {
